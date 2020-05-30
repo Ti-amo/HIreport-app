e24f785ede003ff6dfcb1ac940a730b1a1337ca6
sockonlonton <thehungklk@gmail.com>
2020-05-31 00:24:24 +0700

    Updates

---
+++ precache-manifest.98df115aa238bf5449b2fa77539900e6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd4dc6fef5bbe4947dbde615b08f291",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "ce78e69098dd1a4aaf85",
+    "url": "/material-dashboard-react/static/css/2.656545ef.chunk.css"
+  },
+  {
+    "revision": "074f958b4a8c824e32d3",
+    "url": "/material-dashboard-react/static/css/main.c051d8ca.chunk.css"
+  },
+  {
+    "revision": "ce78e69098dd1a4aaf85",
+    "url": "/material-dashboard-react/static/js/2.24286608.chunk.js"
+  },
+  {
+    "revision": "6bfbeb5394052b98ea733dc8be25c7e8",
+    "url": "/material-dashboard-react/static/js/2.24286608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074f958b4a8c824e32d3",
+    "url": "/material-dashboard-react/static/js/main.a3908514.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/material-dashboard-react/static/js/main.a3908514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe816d7b3554542361f",
+    "url": "/material-dashboard-react/static/js/runtime-main.2c4f3a29.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
